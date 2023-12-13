ca6cb01f6ea030c12ac5826717dd4e4efe8de3f1
Mechanical@307 <chsaikumar307@gmail.com>
2023-12-13 15:41:34 +0530

    saikumar pushed

--- src/JavaBasics/sample9.java
+++ src/JavaBasics/sample9.java
@@ -1,21 +1,23 @@
 package JavaBasics;
 
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 public class sample9 {
+		
 		public static void main(String[] args) {
-			
-		
-	int num = 168;
-	
-	int count = 0;
-	
-	while (num>0) {
-		
-		count++;
-		
-		num =  num/10;
-		
-		
-	}
-	System.out.println(count);
-}
+			String s="madam";
+			String res="";
+			for (int i = s.length()-1; i >=0; i--) {
+				char c = s.charAt(i);
+				res=res+c;
+			}
+		if (s.equals(res)) {
+			System.out.println("palindrome");
+		}
+		else {
+			System.out.println("not a palindrome");
+		}
+		}
 }
---
+++ src/javaProgrammes/datatypes.java
@@ -0,0 +1,24 @@
+package javaProgrammes;
+
+public class datatypes {
+	private void studentId(int num) {
+	}
+	private void studentId(String name) { 
+	}
+	private void studentId(String email, int ph) { 
+	
+	}
+	private void studentId(int dob, String add) { 
+	
+	}
+	public static void main(String[] args) { 
+	datatypes info = new datatypes();
+	info.studentId(123);
+	info.studentId("sai");
+	info.studentId("saikumar@gmail.com", 97);
+	
+	
+	}
+
+
+}
